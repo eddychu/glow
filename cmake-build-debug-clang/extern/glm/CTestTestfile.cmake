35dc46748d1b7a2b4302e630a5ea7f0f1816b330
Eddy Chu <70674478+eddychu@users.noreply.github.com>
2023-05-17 23:33:57 +0800

    remove unused files

--- cmake-build-debug-clang/extern/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/eddy/Projects/glow/extern/glm
-# Build directory: C:/Users/eddy/Projects/glow/cmake-build-debug-clang/extern/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
